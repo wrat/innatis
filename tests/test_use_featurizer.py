d2a10c270191647c0b0b241901c49d5c436923c5
Sam Havens <samhavens@gmail.com>
2018-12-18 00:45:44 -0800

    add USE test

---
+++ tests/test_use_featurizer.py
@@ -0,0 +1,28 @@
+"""
+First run
+
+```sh
+python -m rasa_nlu.train -c sample_configs/config_composite_entities.yml \
+        --data data/examples/dialogflow -o models \
+        --fixed_model_name df-agent --project current --verbose
+```
+
+to train a model on the sample data
+
+"""
+
+from rasa_nlu.model import Interpreter
+import json
+import pytest
+
+
+@pytest.mark.slow
+def test_use_featurizer():
+    interpreter = Interpreter.load("./models/current/df-agent")
+    message = u'I will like some rice and chicken'
+
+    result = interpreter.parse(message)
+    print(json.dumps(result, indent=2))
+
+if __name__ == "__main__":
+    test_use_featurizer()
