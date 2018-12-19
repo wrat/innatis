d89e40e72a1f554952889f520b949298b1832d65
Sam Havens <samhavens@gmail.com>
2018-12-19 13:06:43 -0800

    make setup look more pythony

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'innatis',
-  packages = ['innatis'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A library of useful custom Rasa components',
-  author = 'CarLabs',
-  author_email = 'blake@carlabs.com',
-  url = 'https://github.com/Revmaker/innatis',
-  download_url = 'https://github.com/Revmaker/innatis/tarball/0.1',
-  keywords = ['rasa', 'nlu', 'components'],
-  classifiers = [],
+  name='innatis',
+  packages=['innatis'], # this must be the same as the name above
+  version='0.1',
+  description='A library of useful custom Rasa components',
+  long_description=open('README.md').read(),
+  author='CarLabs',
+  author_email='blake@carlabs.com',
+  url='https://github.com/Revmaker/innatis',
+  download_url='https://github.com/Revmaker/innatis/tarball/0.1',
+  keywords=['rasa', 'nlu', 'components'],
+  classifiers=[]
 )
