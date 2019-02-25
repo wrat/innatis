d2d2c83094070f397ddd8c09a348c693b070267e
Barak <itsbarakyo@gmail.com>
2019-02-25 10:57:52 -0800

    Removed pipfile, added requirements.txt, bumped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                     'tensorflow-hub',
                     'spacy'],
   packages=['innatis', 'innatis.featurizers', 'innatis.extractors'],
-  version='0.4.0',
+  version='0.5.1',
   description='A library of useful custom Rasa components',
   author='CarLabs',
   author_email='blake@carlabs.com',
