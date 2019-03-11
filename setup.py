2771d4807980739d6765d086832c0f666d4adf63
Yuri Golobokov <yuriy.golobokov@icloud.com>
2019-03-11 23:04:36 +0600

    Version bumped to 0.5.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=install_requires,
     packages=['innatis', 'innatis.classifiers', 'innatis.classifiers.bert',
               'innatis.featurizers', 'innatis.extractors'],
-    version='0.5.6',
+    version='0.5.7',
     description='A library of useful custom Rasa components',
     author='CarLabs',
     author_email='blake@carlabs.com',
